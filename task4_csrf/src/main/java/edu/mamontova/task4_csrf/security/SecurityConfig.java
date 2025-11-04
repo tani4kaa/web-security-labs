c6f3a24d47b1f06cccb56d227c50d824fea79d6f
tani4kaa <150384182+tani4kaa@users.noreply.github.com>
2025-11-04 23:15:08 +0200

    Update SecurityConfig.java

--- task4_csrf/src/main/java/edu/mamontova/task4_csrf/security/SecurityConfig.java
+++ task4_csrf/src/main/java/edu/mamontova/task4_csrf/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // Класична поведінка: читаємо токен із запиту під атрибутом "_csrf"
+   
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
 
@@ -39,7 +39,6 @@ public class SecurityConfig {
                         .csrfTokenRequestHandler(requestHandler)
                 )
                 .httpBasic(Customizer.withDefaults())
-                // фільтр, який "торкається" токена, щоб він гарантовано згенерувався і поклався в cookie
                 .addFilterAfter(new CsrfCookieTouchFilter(), org.springframework.security.web.authentication.www.BasicAuthenticationFilter.class);
 
         return http.build();
